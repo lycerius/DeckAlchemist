2981ca197875e12a74c664e441dcfb452f12f8ad
hypereddie <ecp4224@gmail.com>
2018-02-20 11:47:23 -0500

    Add equality members

--- DeckAlchemist.Api/Objects/Mtg/Decks/MongoMtgDeck.cs
+++ DeckAlchemist.Api/Objects/Mtg/Decks/MongoMtgDeck.cs
@@ -28,5 +28,29 @@ namespace DeckAlchemist.Api.Objects.Mtg.Decks
         {
             return DeckCompare.Compare(this, other);
         }
+
+        protected bool Equals(MongoMtgDeck other)
+        {
+            return string.Equals(Name, other.Name) && Meta.Equals(other.Meta) && Equals(Cards, other.Cards);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MongoMtgDeck) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ Meta.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Cards != null ? Cards.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
     }
 }
--- DeckAlchemist.Api/Objects/Mtg/Decks/MtgDeckCard.cs
+++ DeckAlchemist.Api/Objects/Mtg/Decks/MtgDeckCard.cs
@@ -17,5 +17,26 @@
                 return Name.GetHashCode();
             }
         }
+
+        protected bool Equals(MtgDeckCard other)
+        {
+            return string.Equals(Name, other.Name) && Count == other.Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((MtgDeckCard) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Name != null ? Name.GetHashCode() : 0) * 397) ^ Count;
+            }
+        }
     }
 }
