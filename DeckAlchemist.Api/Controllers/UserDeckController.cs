3e5ac511ceb30c009033571b317e167a5870ffd2
Tom <tom.plano10@gmail.com>
2018-02-23 17:05:35 -0500

    Added mongo userdeck source to interface with the userdeck controller

--- DeckAlchemist.Api/Controllers/UserDeckController.cs
+++ DeckAlchemist.Api/Controllers/UserDeckController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DeckAlchemist.Api.Sources.UserDeck;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,6 +12,14 @@ namespace DeckAlchemist.Api.Controllers
     [Route("api/[controller]")]
     public class UserDeckController : Controller
     {
+
+        readonly IUserDeckSource _source;
+
+        public UserDeckController(IUserDeckSource source)
+        {
+            _source = source;
+        }
+
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
