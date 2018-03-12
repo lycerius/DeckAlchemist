9d950b4dc82210609db39dbff5e936936d5691c1
lycerius <purpuranc@gmail.com>
2018-03-12 17:19:04 -0400

    Login controller work

---
+++ DeckAlchemist.Api/Auth/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace DeckAlchemist.Api.Auth
+{
+    public static class UserInfo
+    {
+        public static string Id(ClaimsPrincipal user) => user.FindFirst("sub")?.Value;
+        public static string Email(ClaimsPrincipal user) => user.FindFirst("email")?.Value;
+        
+    }
+}
---
+++ DeckAlchemist.Api/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DeckAlchemist.Api.Auth;
+using DeckAlchemist.Api.Sources.Collection;
+using DeckAlchemist.Api.Sources.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DeckAlchemist.Api.Controllers
+{
+    [Authorize(Policy = "Email")]
+    [Route("api/login")]
+    public class LoginController : Controller
+    {
+        ICollectionSource _collectionSource;
+        IUserSource _userSource;
+
+        [HttpGet]
+        public string Login()
+        {
+            var userInfo = HttpContext.User;
+
+            //Check to see if the user is created (first time login)
+            CreateUserIfNotExist(userInfo);
+
+
+            //Check to see if the user's collection is create
+            CreateCollectionIfNotExist(userInfo);
+        }
+
+        void CreateUserIfNotExist(ClaimsPrincipal user)
+        {
+            var userId = UserInfo.Id(userInfo);
+            if (!_userSource.UserExists(userId))
+            {
+                var email = UserInfo.Email(userInfo);
+                _userSource.CreateUser(userId, email);
+            }
+        }
+
+        void CreateCollectionIfNotExist(ClaimsPrincipal user)
+        {
+            var userId = UserInfo.Id(user);
+            if(!_collectionSource.ExistsForUser(userId))
+            {
+                var user = _userSource.GetUser(userId);
+                var collectionId = _collectionSource.CreateCollection(userId);
+
+                _userSource.SetCollection(userId, collectionId);
+            }
+        }
+
+    }
+}
