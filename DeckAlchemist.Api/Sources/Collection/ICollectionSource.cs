13959afd39eca0410fb3596346bd5b938ff7c30a
Tom <tom.plano10@gmail.com>
2018-04-02 12:49:20 -0400

    more fixes

--- DeckAlchemist.Api/Sources/Collection/ICollectionSource.cs
+++ DeckAlchemist.Api/Sources/Collection/ICollectionSource.cs
@@ -8,8 +8,8 @@ namespace DeckAlchemist.Api.Sources.Collection
 {
     public interface ICollectionSource
     {
-        bool AddCardToCollection(string uId, IList<string> cardNames);
-        bool RemoveCardFromCollection(string uId, IList<string> cardNames);
+        bool AddCardToCollection(string uId, IList<string> cardName);
+        bool RemoveCardFromCollection(string uId, IList<string> cardName);
         bool MarkCardAsLent(string uId, IList<string> cardName);
         bool AddCardAsLent(string uId, IList<string> cardName);
         void Init();
