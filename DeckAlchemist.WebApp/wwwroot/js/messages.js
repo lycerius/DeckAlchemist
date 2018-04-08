dbb5a9938ea44aa467fd2cd3ff51f189441bd4e6
lycerius <purpuranc@gmail.com>
2018-04-08 15:33:21 -0400

    Better interface

--- DeckAlchemist.WebApp/wwwroot/js/messages.js
+++ DeckAlchemist.WebApp/wwwroot/js/messages.js
@@ -7,42 +7,68 @@
 function populateInbox(messagesJson) {
     var inboxContainer = $("#inbox-content")
     var messageContainer = $("<div></div>")
+    var list = $("<ul class='list-group'></ul>")
     $.each(messagesJson, function(index){
         var message = messagesJson[index];
         var subject = message['subject']
-        var entry = $("<p>"+subject+"</p>")
+
+        var entry = $("<li class='list-group-item'>"+subject+"</li>")
         entry.click(function(e) {
             displayMessageContent(message);
         })
-        messageContainer.append(entry);
+
+        entry.mouseover(function(e) {
+            
+            entry.addClass("active")
+        })
+
+        entry.mouseout(function(e){
+            entry.removeClass("active")
+        })
+        list.append(entry)
+
     })
+    messageContainer.append(list);
     inboxContainer.empty();
     inboxContainer.append(messageContainer);
 }
 
 function displayMessageContent(message) {
-    var messageBody = $("#message-body-content")
+    var messageBodyDiv = $("#message-body-content")
     var actionBar = $("#message-action-bar")
-    messageBody.text(message["body"])
+    var messageCard = $("<div class='card'></div>")
+    var cardBody = $("<div class='card-body'></div>")
+    var messageTitle = $("<h5 class='card-title' style='width:4rem'>"+message["subject"]+"</h5>")
+    var messageBody = $("<p class='card-text'>"+message["body"]+"</p>")
+    var messageSender = $("<h6 class='card-subtitle mb-2 text-muted'>"+message["senderId"]+"</h6>")
+   
+    cardBody.append(messageTitle);
+    cardBody.append(messageSender);
+    cardBody.append(messageBody);
+
     if(message["type"] == "Loan") {
         actionBar.empty();
-        var acceptLoanButton = $("<button class='form-control btn'>Accept</button>")
+        var acceptLoanButton = $("<a href='#' class='card-link'>Accept</button>")
         acceptLoanButton.click(function(e){
             acceptLoanRequest(message["messageId"]).then(function() {
                 swal("Accepted")
             })
         })
-        actionBar.append(acceptLoanButton)
+        cardBody.append(acceptLoanButton)
     } else if(message["type"] == "Group") {
         actionBar.empty();
-        var acceptInviteButton = $("<button class='form-control btn'>Accept</button>")
+        var acceptInviteButton = $("<a href='#' class='card-link'>Accept</button>")
         acceptInviteButton.click(function(e) {
             acceptGroupInvite(message["messageId"]).then(function() {
                 swal("Now in group")
             })
         })
-        actionBar.append(acceptInviteButton)
+        cardBody.append(acceptInviteButton)
     }
+
+    messageCard.append(cardBody)
+    messageBodyDiv.empty();
+    messageBodyDiv.append(messageCard);
 }
 
 $(document).ready(function() {
