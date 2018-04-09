a45ba63ff89f91cdb93ca0d326c57fd05535d997
lycerius <purpuranc@gmail.com>
2018-04-09 10:56:16 -0400

    User should only see lendable cards when seeking a loan

--- DeckAlchemist.Api/Controllers/CollectionsController.cs
+++ DeckAlchemist.Api/Controllers/CollectionsController.cs
@@ -57,10 +57,14 @@ namespace DeckAlchemist.Api.Controllers
         {
             var result = _collectionSource.GetCollection(otherUID);
             if (result == null || result.OwnedCards == null || result.BorrowedCards == null) return null;
-            var cardInfo = GetCardInfo(result.OwnedCards.Keys);
+
+            //Only lendable cards should be here
+            var ownedCards = result.OwnedCards;
+            var lendableOwnedCards = new Dictionary<string, IOwnedCard>(ownedCards.Where(card => card.Value.Lendable));
+            var cardInfo = GetCardInfo(lendableOwnedCards.Keys);
             var model = new OwnedCardsModel
             {
-                OwnedCards = result.OwnedCards,
+                OwnedCards = lendableOwnedCards,
                 CardInfo = cardInfo
             };
             return model;
